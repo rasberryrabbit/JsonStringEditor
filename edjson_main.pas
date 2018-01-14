90c600ea8fcb7c632a0fd1657f8371bc5e2164c9
rasberryrabbit <lovelust@hanmail.net>
2018-01-14 14:19:16 +0900

    Add translate

--- edjson_main.pas
+++ edjson_main.pas
@@ -13,6 +13,7 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    ActionTranslate: TAction;
     ActionImportNode: TAction;
     ActionImport: TAction;
     ActionPrev: TAction;
@@ -20,6 +21,7 @@ type
     ActionList1: TActionList;
     Button2: TButton;
     Button3: TButton;
+    ComboBoxtoLang: TComboBox;
     FileExit1: TFileExit;
     FileOpen1: TFileOpen;
     FileSaveAs1: TFileSaveAs;
@@ -29,6 +31,8 @@ type
     MenuItem1: TMenuItem;
     MenuItem10: TMenuItem;
     MenuItem11: TMenuItem;
+    MenuItem12: TMenuItem;
+    MenuItem13: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
@@ -47,11 +51,13 @@ type
     procedure ActionImportNodeExecute(Sender: TObject);
     procedure ActionNextExecute(Sender: TObject);
     procedure ActionPrevExecute(Sender: TObject);
+    procedure ActionTranslateExecute(Sender: TObject);
     procedure FileOpen1Accept(Sender: TObject);
     procedure FileSaveAs1Accept(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure FormShow(Sender: TObject);
     procedure Memo1Exit(Sender: TObject);
     procedure TreeView1DblClick(Sender: TObject);
     procedure TreeView1Enter(Sender: TObject);
@@ -79,7 +85,7 @@ implementation
 {$R *.lfm}
 
 uses
-  jsonparser, windows;
+  jsonparser, windows, uGoogleTranApi, LazUTF8;
 
 var
   koData : TJSONData = nil;
@@ -211,6 +217,21 @@ begin
   end;
 end;
 
+procedure TForm1.ActionTranslateExecute(Sender: TObject);
+var
+  soutput: string;
+begin
+  if not Memo1.Enabled then
+    exit;
+  if Memo1.SelLength>0 then begin
+    soutput:=GoogleTranAPI_Translate('',ComboBoxtoLang.Text,pchar(Memo1.SelText));
+    Memo1.SelText:=soutput+' / '+Memo1.SelText;
+  end else begin
+    soutput:=GoogleTranAPI_Translate('',ComboBoxtoLang.Text,pchar(Memo1.Text));
+    Memo1.Text:=soutput+' / '+Memo1.Text;
+  end;
+end;
+
 procedure TForm1.FileSaveAs1Accept(Sender: TObject);
 begin
   SaveJson(pchar(FileSaveAs1.Dialog.FileName),koData);
@@ -234,6 +255,15 @@ end;
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
   FreeAndNil(koData);
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  lid : string;
+begin
+  GoogleTranAPI_GetLangs(ComboBoxtoLang.Items);
+  LazGetShortLanguageID(lid);
+  ComboBoxtoLang.Text:=lid;
 end;
 
 procedure TForm1.Memo1Exit(Sender: TObject);
@@ -301,13 +331,13 @@ begin
     case Data.JSONType of
     jtObject: begin
                for i:=0 to Data.Count-1 do begin
-                 N:=TreeView1.Items.AddChildObject(pa,TJSONObject(Data).Names[i],TJSONObject(Data).Items[i]);
+                 N:=TreeView1.Items.AddChildObject(pa,pchar(TJSONObject(Data).Names[i]),TJSONObject(Data).Items[i]);
                  AddJsonData(N,TJSONObject(Data).Items[i]);
                end;
               end;
     jtArray : begin
                for i:=0 to Data.Count-1 do begin
-                 N:=TreeView1.Items.AddChildObject(pa,TJSONArray(Data).Strings[i],TJSONArray(Data).Items[i]);
+                 N:=TreeView1.Items.AddChildObject(pa,pchar(TJSONArray(Data).Strings[i]),TJSONArray(Data).Items[i]);
                end;
               end;
     jtNull:   begin
@@ -324,12 +354,26 @@ begin
 end;
 
 procedure TForm1.MemoDoChanges;
+var
+  unode:TTreeNode;
+  doUpdateLabel: Boolean;
 begin
   if Memo1.Modified and (pnode<>nil) then begin
+    doUpdateLabel:=False;
+    unode:=pnode.Parent;
+    if unode<>nil then
+      doUpdateLabel:=TJSONData(unode.Data).JSONType=jtArray;
     case TJSONData(pnode.Data).JSONType of
-    jtString: TJSONString(pnode.Data).AsUnicodeString:= UTF8Decode(
-      Memo1.Lines.Text);
-    jtNumber: TJSONNumber(pnode.Data).AsString:= Memo1.Lines.Text;
+    jtString: begin
+                TJSONString(pnode.Data).AsUnicodeString:= UTF8Decode(Memo1.Lines.Text);
+                if doUpdateLabel then
+                  pnode.Text:=pchar(Memo1.Lines.Text);
+              end;
+    jtNumber: begin
+                TJSONNumber(pnode.Data).AsString:= Memo1.Lines.Text;
+                if doUpdateLabel then
+                  pnode.Text:=pchar(Memo1.Lines.Text);
+              end;
     end;
     JsonModified:=True;
     Memo1.Modified:=False;
